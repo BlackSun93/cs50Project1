1b4326267e55ead6444aca48f0f961e37b46b864
BlackSun93 <54064095+BlackSun93@users.noreply.github.com>
2020-06-24 15:16:52 +0100

    version 1.0
    Ready for submission, html pages need more formatting

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cs50Project1
-project 1 of cs50 course
+# Project 1
+
+Web Programming with Python and JavaScript
+
+This repo contains the files required for a website that allows a user to search for books and leave reviews.
+import.py allows books.csv to be uploaded as a sql database hosted by heroku.
+application.py contains flask app.routes for a locally hosted website where users can register, login, search for books, leave reviews and when querying the /api/<some-isbn> route, will query the goodreads API and return a json with goodreads data.
+ 
--- application.py
+++ application.py
@@ -1,16 +1,16 @@
 import os
-import psycopg2
+import psycopg2, requests, json
 from tables import TableMake
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect, url_for, jsonify, flash
 from flask_session import Session
-from sqlalchemy import create_engine, MetaData, Table, Column, String
+from sqlalchemy import create_engine, MetaData, Table, Column, String, select
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "goodluck"
 
 # Check for environment variable
-#os.putenv("DATABASE_URL", "postgres://qhpnyexenogece:1c04bab21940e673a432d6354cc67440b4d2830e4213f84f18fddf392ff88b9e@ec2-52-202-22-140.compute-1.amazonaws.com:5432/d8tpvv7b4aadb")
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
@@ -23,111 +23,226 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-#metadata = MetaData()
-#users = Table('users', metadata,
-#Column ('userID', Integer, primary_key=True),
-#Column ('username', String(20), nullable=false),
-#Column ('password', String(20), nullable=false)
-#)
+createReviews = ('CREATE TABLE IF NOT EXISTS "reviews" ('
+    ' revid SERIAL PRIMARY KEY,'
+    ' isbn VARCHAR REFERENCES books,'
+    ' userid INTEGER REFERENCES users,'
+    ' review VARCHAR,'
+    ' rating INTEGER);')
 
-#users.create(engine, checkfirst=True)
+db.execute (createReviews)
+db.commit()
 
-#db.execute("IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'users'))
- #  
-  # 
-   #
-   #BEGIN
-    #  PRINT 'Database Table Exists'
-   #END;
-#ELSE
- #  BEGIN
-  #    PRINT 'No Table in database'
-   #END;  )
+createUsers = ('CREATE TABLE IF NOT EXISTS "users" ('
+    'username VARCHAR NOT NULL,'
+    'password VARCHAR NOT NULL,'
+    'userid SERIAL UNIQUE,'
+    'PRIMARY KEY (userID));')
 
+db.execute (createUsers)
+db.commit()
 
-@app.route("/")
+@app.route("/home", methods = ["POST", "GET"])
+@app.route("/", methods = ["POST", "GET"])
+
 def index():
-    headline = " Headline"
-    body = "Hello this is a body"
-    return render_template ("index.html", headline=headline)
+    if request.method == 'GET':
+            if 'USER' in session:
+                return redirect(url_for("search", name = session['NAME']))
+            else:    
+                headline = "Please try signing in or sign up"
+                body = "Hello this is a body"
+                return render_template ("index.html", headline=headline)
 
+    if request.method == 'POST':
+        usernamelog = request.form['usernamelog']
+        passwordlog = request.form['passwordlog']
+
+        if not usernamelog or not passwordlog:
+            return render_template ("index.html", headline="Incorrect username or password")
+    
+        for i in usernamelog:
+            if i == "'" or i.isspace():
+                return render_template ("index.html", headline= "not a valid username")
+    
+        for i in passwordlog:
+            if i == "'" or i.isspace():
+                return render_template ("index.html", headline= "not a password")
+
+        databaseQuery = db.execute('SELECT * FROM users WHERE (username = :usernamelog AND password = :passwordlog)',
+        {"usernamelog": usernamelog, "passwordlog": passwordlog}).fetchone()
+
+        if databaseQuery:
+            session['USER'] = databaseQuery.userid
+            session['NAME'] = databaseQuery.username.capitalize()
+            return render_template ("search.html", name = session['NAME'])
+        else:
+            return render_template ("index.html", headline="Incorrect username or password")
 
 
 @app.route('/signup')
 def signup():
-    
-    #engine.execute ('CREATE TABLE IF NOT EXISTS "users" ('
-     #  'username VARCHAR NOT NULL,'
-      #  'password VARCHAR NOT NULL,'
-       # 'userID VARCHAR NOT NULL,'
-        #'PRIMARY KEY (userID));')
 
-    command = ('CREATE TABLE IF NOT EXISTS "users" ('
-       'username VARCHAR NOT NULL,'
-       'password VARCHAR NOT NULL,'
-        'userID SERIAL UNIQUE,'
-        'PRIMARY KEY (userID));')
-
-    db.execute (command)
-    db.commit()
-
-    #print ("Table made")
     return render_template ("signup.html")
 
-@app.route("/signupcomplete", methods = ['POST'])
+@app.route("/signupcomplete", methods = ['POST', 'GET'])
 def signupcomplete():
 
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'GET':
+        return render_template("index.html")
 
-    if username == "" or password == "":
-        headline = "Please complete both fields"
-        return render_template ("index.html", headline=headline)
-    
     else:
 
-        db.execute ("INSERT INTO users (username, password) VALUES (:username, :password)",
-         {"username": username, "password": password})
-        db.commit()
+        username = request.form['username']
+        password = request.form['password']
 
-        headline = "Signup Successful"
-        return render_template ("index.html", headline=headline)
+        for i in username:
+            if i == "'" or i.isspace():
+                return redirect(url_for('signup', headline="Please use a valid username"))
+        
+        for i in password:
+            if i == "'" or i.isspace():
+                return redirect(url_for('signup', headline="Please use a valid password"))
+            else:
+                continue
 
-@app.route("/loginsuccess", methods = ['POST'])
-def loginsuccess():
-    
-    usernamelog = request.form['usernamelog']
-    passwordlog = request.form['passwordlog']
+        if username == "" or password == "":
+            headline = "Please complete both fields"
+            return render_template("signup.html", headline="Please use a valid password")
+        
+        elif db.execute("SELECT username FROM users WHERE username = :username", {"username": username}).rowcount != 0:
+            headline = "This username is taken"
+            return render_template("signup.html", headline=headline)
 
-    #logcommand = ('SELECT * FROM users WHERE (username = 'usernamelog' AND password = 'passwordlog')')
+        else:
 
-    if db.execute('SELECT * FROM users WHERE (username = :usernamelog AND password = :passwordlog)', {"usernamelog": usernamelog, "passwordlog": passwordlog}).rowcount == 1:
+            db.execute ("INSERT INTO users (username, password) VALUES (:username, :password)",
+            {"username": username, "password": password})
+            db.commit()
 
-    #if login == 1
-        return render_template ("search.html")
-    
-    else:
-        return render_template ("index.html", headline="Username or password incorrect")
-
+            headline = "Signup Successful"
+            return render_template("index.html", headline = headline)
 
 @app.route("/logout")
 def logout():
-    
-    return "Project 1: TODO"
+    if 'USER' in session:
+        session.pop('USER',None)
+        session.pop('NAME', None)
 
+        return render_template("index.html", headline="logged out")
 
-@app.route("/search")
-def search():
-    
-    return "Project 1: TODO"
-
-
-@app.route("/hello", methods=["GET", "POST"])
-def hello():
-    if request.method == "GET":
-        return "Please submit form properly"
     else:
-        name = request.form.get("name")
-        return render_template("index.html", headline=name)
+        return render_template("index.html", headline="You are already logged out")
+
+
+@app.route("/search", methods = ['POST', 'GET'])
+def search():
+    if request.method == 'GET':
+        #if logged in, returns the search page, otherwise takes user back to the main page
+        if 'USER' in session:
+            return render_template("search.html", name = session['NAME'])
+
+        else:
+            return render_template("index.html", headline="You have to login first to see the search page")
+
+    #if post request submitted from a form being completed, attemps to convert all the form inputs into valid sqlAlchemy syntax
+    #by adding a % to the end of the input (as SQL query is a LIKE query), if nothing was entered into the form, sets variable to ""
+    #and correct sql statement is picked out.
+    else:
+        try:
+            isbn = request.form['isbn'] + "%"
+        except:
+           isbn = ""
+        try:
+            title = request.form['title'].capitalize() + "%"
+        except:
+            title = ""
+        try:
+            author = request.form['author'] + "%"
+        except:
+            author = ""
+        
+    if isbn != "":
+        books = db.execute("SELECT * FROM BOOKS WHERE (isbn LIKE :isbn)", {"isbn": isbn})
+
+    elif title != "":
+        books = db.execute("SELECT * FROM BOOKS WHERE (title LIKE :title)", {"title": title})
+       
+    else:
+        books = db.execute("SELECT * FROM BOOKS WHERE (author LIKE :author)", {"author": author})
+
+    if books.rowcount == 0:
+        return render_template("search.html", headline = "No books found with those parameters") 
+        
+    else:
+        return render_template("booksearchsuccess.html", books=books)
+
+
+@app.route("/selectedbook/<string:isbn>", methods = ["GET", "POST"])
+def selectedbook(isbn):
+    apireq = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "qjfi7EyD90v3MlEghQNbBQ", "isbns": isbn})
+    books = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn})
+    reviews = db.execute("SELECT reviews.review, reviews.rating, users.username FROM reviews "
+                        "INNER JOIN users ON reviews.userid = users.userid "
+                        "WHERE isbn = :isbn", {"isbn": isbn})
+    
+    apiresult = apireq.json()
+    
+    rev_count =  apiresult['books'][0]['reviews_count']
+    average_score = apiresult['books'][0]['average_rating']
+
+    if request.method =="GET":     
+        return render_template("selectedbook.html", books = books, reviews = reviews, rev_count = rev_count, average_score = average_score)
+
+    else:
+        rating = request.form['rating']
+        rev = request.form['review']
+        current = session["USER"]
+       
+        if db.execute ("SELECT * FROM reviews WHERE userid = :userid AND isbn = :isbn",
+         {"userid": current, "isbn": isbn}).rowcount != 0:
+
+            db.execute("UPDATE reviews SET review = :review, rating = :rating WHERE userid = :userid AND isbn = :isbn", 
+            {"review": rev, "userid": current, "isbn": isbn, "rating": rating})
+            db.commit()
+
+            flash('Your review has been updated! Please refresh the page to see it displayed.')
+            
+            return render_template("selectedbook.html",  books = books, reviews = reviews, rev_count = rev_count, average_score = average_score)
+
+        else:
+            db.execute ("INSERT INTO reviews (isbn, userid, review, rating) VALUES (:isbn, :userid, :review, :rating)",
+            {"isbn": isbn, "userid": current, "review": rev, "rating": 5})
+            db.commit()
+            flash('Your review has been submitted! It will be visible once you refresh the page')
+            return render_template("selectedbook.html", books = books, reviews = reviews, rev_count = rev_count, average_score = average_score)
+
+@app.route("/api/<string:isbn>")
+def api(isbn):
+    
+    apireq = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "qjfi7EyD90v3MlEghQNbBQ", "isbns": isbn})
+
+    if not apireq:
+        return render_template("index.html", headline="404 book not found")
+
+    else:
+        dbreq = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    
+        apiresult = apireq.json()
+
+        builtjson = {
+    "title": dbreq.title,
+    "author": dbreq.author,
+    "year": dbreq.year,
+    "isbn": dbreq.isbn,
+    "review_count": apiresult['books'][0]['reviews_count'],
+    "average_score": apiresult['books'][0]['average_rating']
+    }
+
+        return jsonify(builtjson)
+
+        
+            
+    
 
     
